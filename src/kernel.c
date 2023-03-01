98f2528455bb50aa64b51a633b51987e8b128907
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-03-01 13:07:49 +0700

    check

--- src/kernel.c
+++ src/kernel.c
@@ -16,7 +16,7 @@ void kernel_setup(void) {
     framebuffer_write(3, 9,  'a', 0, 0xF);
     framebuffer_write(3, 10, 'i', 0, 0xF);
     framebuffer_write(3, 11, '!', 0, 0xF);
-    write_splash_screen();
+    write_splash_screen3();
     framebuffer_set_cursor(3, 9);
     while (TRUE);
     while (TRUE) b += 1;
@@ -48,3 +48,29 @@ void write_splash_screen() {
 //     framebuffer_write(20, 0, "                     44  22           00       00                   ", 2, 0);
 //     framebuffer_write(21, 0, "                     44  22222222222    0000000                     ", 2, 0);
 // }
+
+void write_splash_screen3() {
+    char splash_screen[] = "BBBBB     RRRRRR      OOOOOO   TTTTTTTT  HH    HH  EEEEEEE  RRRRRR  \n"
+                           "BB   BB   RR    RR   OO     OO    TT     HH    HH  EE       RR   RR \n"
+                           "BBBBBB    RRRRRR     OO     OO    TT     HHHHHHHH  EEEEE    RRRRRR  \n"
+                           "BB   BB   RR   RR    OO     OO    TT     HH    HH  EE       RR   RR \n"
+                           "BBBBB     RR    RR    OOOOOO      TT     HH    HH  EEEEEEE  RR    RR\n"
+                           "            44       44  22222222222    0000000                     \n"
+                           "            44       44  22       22  00       00                   \n"
+                           "            44       44  22       22  00       00                   \n"
+                           "            44       44           22  00       00                   \n"
+                           "            44444444444  22222222222  00       00                   \n"
+                           "                     44  22           00       00                   \n"
+                           "                     44  22222222222    0000000                     \n";
+
+    int row = 10, col = 30;
+    for (int i = 0; i < strlen(splash_screen); i++) {
+        if (splash_screen[i] == '\n') {
+            row++;
+            col = 30;
+        } else {
+            framebuffer_write(row, col, splash_screen[i], 2, 0);
+            col++;
+        }
+    }
+}
