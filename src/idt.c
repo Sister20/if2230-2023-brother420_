696e683e6f3b63cf265feb4c8fdee476b5242fac
Hidayatullah Wildan Ghaly Buchary <16521502@std.stei.itb.ac.id>
2023-03-23 14:38:21 +0700

    not fix: idt gate

--- src/idt.c
+++ src/idt.c
@@ -2,6 +2,7 @@
 
 extern struct IDT idt;
 extern struct IDTR idtr;
+extern struct interrupt_descriptor_table interrupt_descriptor_table;
 
 void initialize_idt(void) {
     /* TODO :
@@ -29,5 +30,10 @@ void set_interrupt_gate(uint8_t int_vector, void *handler_address, uint16_t gdt_
     idt_int_gate->_r_bit_2    = INTERRUPT_GATE_R_BIT_2;
     idt_int_gate->_r_bit_3    = INTERRUPT_GATE_R_BIT_3;
     idt_int_gate->gate_32     = 1;
-    idt_int_gate->valid_bit   = 1;
+    idt_int_gate->privilege   = privilege;
+    idt_int_gate->_reserved   = 0;
+    idt_int_gate->offset_low  = (uint32_t)handler_address & 0xFFFF;
+    idt_int_gate->offset_high = ((uint32_t)handler_address >> 16) & 0xFFFF;
+    idt_int_gate->segment     = gdt_seg_selector;
+    idt_int_gate->present     = 1;
 }
