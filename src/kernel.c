ace1d702c2f648406f7a401ec797aa623647267a
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-04-03 22:20:55 +0700

    fix: kernel.c update kernel for debugging

--- src/kernel.c
+++ src/kernel.c
@@ -19,24 +19,25 @@ void kernel_setup(void) {
     pic_remap();
     initialize_idt();
     activate_keyboard_interrupt();
+    keyboard_state_activate();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
     initialize_filesystem_fat32();
     gdt_install_tss();
     set_tss_register();
-
+    framebuffer_write(0,0,sizeof(struct FAT32DirectoryEntry),0xe, 0);
     // Allocate first 4 MiB virtual memory
     allocate_single_user_page_frame((uint8_t*) 0);
 
     // Write shell into memory (assuming shell is less than 1 MiB)
     struct FAT32DriverRequest request = {
         .buf                   = (uint8_t*) 0,
-        .name                  = "shell",
+        .name                  = "shells",
         .ext                   = "\0\0\0",
         .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 0x100000,
+        .buffer_size           = 0x1000,
     };
-    read(request);
+    write(request);
 
     // Set TSS $esp pointer and jump into shell 
     set_tss_kernel_current_stack();
