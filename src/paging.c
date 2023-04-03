47007675f024660b8b5aa7c610d0f8d57eb728d6
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-04-03 15:00:29 +0700

    fix: unfinished allocate_singl_ HELP BOOTLOOP T_T

--- src/paging.c
+++ src/paging.c
@@ -30,28 +30,37 @@ void update_page_directory_entry(void *physical_addr, void *virtual_addr, struct
     flush_single_tlb(virtual_addr);
 }
 
+/* Curiga salah di sini, help T_T BOOTLOOP */
 int8_t allocate_single_user_page_frame(void *virtual_addr) {
     // Using default QEMU config (128 MiB max memory)
     uint32_t last_physical_addr = (uint32_t) page_driver_state.last_available_physical_addr;
     
-    struct PageDirectoryEntryFlag flag = {
-        .present_bit        = 1,
-        .write_bit          = 1,
-        .userOrSupervisor   = 1,
-        .use_pagesize_4_mb  = 1
-    };
+    // TODO : Allocate Page Directory Entry with user privilege
 
-    if (last_physical_addr >= 32 * PAGE_FRAME_SIZE) {
+    struct PageDirectoryEntry page_dir_entry = {
+        .flag.present_bit           = 1,
+        .flag.write_bit             = 1,
+        .flag.userOrSupervisor      = 1,
+        .flag.PWT                   = 0,
+        .flag.PCD                   = 0,
+        .flag.accessed_bit          = 0,
+        .flag.dirty_bit             = 0,
+        .flag.use_pagesize_4_mb     = 1,
+        .lower_address              = last_physical_addr >> 22 & 0x3FF
+    };
+    
+    if (last_physical_addr >= PAGE_FRAME_SIZE) {
         return -1;
     } else {
-        update_page_directory_entry((void*) last_physical_addr, virtual_addr, flag);
+        // Update page directory entry
+        update_page_directory_entry(&last_physical_addr, virtual_addr, page_dir_entry.flag);
         page_driver_state.last_available_physical_addr += PAGE_FRAME_SIZE;
         return 0;
     }
     
-    // TODO : Allocate Page Directory Entry with user privilege
     return -1;
 }
+
 
 void flush_single_tlb(void *virtual_addr) {
     asm volatile("invlpg (%0)" : /* <Empty> */ : "b"(virtual_addr): "memory");
